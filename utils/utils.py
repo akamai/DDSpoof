1e4322220e32d3d63b98137aac45eba24d671a7b
Ori David <ori.david@guardicore.com>
2024-02-22 14:21:18 +0200

    added dhcp relay agent support and dhcp_coerce

--- utils/utils.py
+++ utils/utils.py
@@ -9,3 +9,10 @@ def get_random_string(length: int, character_set: str = string.ascii_lowercase) 
 def get_random_hex_string(length: int) -> str:
     character_set = "0123456789abcdef"
     return get_random_string(length, character_set)
+
+
+def ip_to_bytes(ip: str) -> bytes:
+    ip_bytes = b''
+    for oct in ip.split("."):
+        ip_bytes += int(oct).to_bytes(1, 'little')
+    return ip_bytes
